764e36199cfe2aadcc6b957981e2f5ee24270f03
Saga-sanga <vulcan248@gmail.com>
2023-03-06 14:02:29 +0530

    First commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,36 @@
-import type { NextPage } from 'next'
-import { useState } from 'react'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import AddressForm from '../components/AddressForm'
+import type { NextPage } from 'next';
+import { useState } from 'react';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import AddressForm from '../components/AddressForm';
+import * as web3 from '@solana/web3.js';
 
 const Home: NextPage = () => {
-  const [balance, setBalance] = useState(0)
-  const [address, setAddress] = useState('')
+  const [balance, setBalance] = useState(0);
+  const [address, setAddress] = useState('');
+  const [executable, setExecutable] = useState(false);
 
   const addressSubmittedHandler = (address: string) => {
-    setAddress(address)
-    setBalance(1000)
-  }
+    try {
+      const key = new web3.PublicKey(address);
+      setAddress(key.toBase58());
+
+      const connection = new web3.Connection(web3.clusterApiUrl('devnet'));
+
+      connection.getAccountInfo(key).then(response => {
+        setExecutable(response?.executable ?? false);
+      });
+
+      connection.getBalance(key).then(balance => {
+        setBalance(balance / web3.LAMPORTS_PER_SOL);
+      });
+    } catch(error) {
+      setAddress('');
+      setBalance(0);
+      alert(error);
+    }
+  };
 
   return (
     <div className={styles.App}>
@@ -23,9 +41,10 @@ const Home: NextPage = () => {
         <AddressForm handler={addressSubmittedHandler} />
         <p>{`Address: ${address}`}</p>
         <p>{`Balance: ${balance} SOL`}</p>
+        <p>{`Account executable: ${executable ? 'Yep!' : 'Nope!'}`}</p>
       </header>
     </div>
   )
 }
 
-export default Home
+export default Home;
